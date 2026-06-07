fd7bb92e5744fe3b1f79b7a589929cd851401d77
Farse Fahmy <faresfahmysaeedahmed@gmail.com>
2026-06-07 08:02:32 +0300

    Update Hero component with new images and titles

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -16,12 +16,12 @@ export default function Hero({results}:{results:any[]}) {
     card:(
         <>
         <MotionItem initial={{opacity:0}} whileInView={{ opacity:1,transition:{duration:0.9} }}>
-        <Image src={`https://image.tmdb.org/t/p/original/8ZTVqvhhQHmbAoRC6u6Xbpldv0g.jpg`} fill alt='s1' className='object-cover'/>
+        <Image src={`https://image.tmdb.org/t/p/original/zMwhWailP1WY7sb6AoE6b8ugoy.jpg`} fill alt='s1' className='object-cover'/>
         </MotionItem>
         <CardInfo
-        title={`Inception`}
-        desc={`A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project.`}
-        id={27205}
+        title={`Swapped`}
+        desc={`A small woodland creature and a majestic bird, two natural sworn enemies of the Valley, magically trade places and set off on an adventure of a lifetime to switch back. Their journey soon uncovers a greater threat—one that could endanger not only their species, but the entire valley they call home.`}
+        id={1007757}
         />
         </>
   )},
@@ -29,12 +29,12 @@ export default function Hero({results}:{results:any[]}) {
     card:(
         <>
         <MotionItem initial={{opacity:0}} whileInView={{ opacity:1,transition:{duration:0.9} }}>
-        <Image src={`https://image.tmdb.org/t/p/original/p2fX6wUnj72Z9w3647Yw67B3vRk.jpg`} fill  alt='s1' className='object-cover'/>
+        <Image src={`https://image.tmdb.org/t/p/original/7TJjD2X9GTEqyLVIJKLS85J2V47.jpg`} fill  alt='s1' className='object-cover'/>
         </MotionItem>
         <CardInfo
-        title={`Interstellar`}
-        desc={`The adventures of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.`}
-        id={157336}
+        title={`Madness`}
+        desc={`An escaped convicted murderer invades the cottage of a man, his wife and the wife's sister, whereupon he proceeds to torment this already dysfunctional trio with rape and violence.`}
+        id={28322}
         />
         </>
   )},
@@ -42,12 +42,12 @@ export default function Hero({results}:{results:any[]}) {
     card:(
         <>
         <MotionItem initial={{opacity:0}} whileInView={{ opacity:1,transition:{duration:0.9} }}>
-        <Image src={`https://image.tmdb.org/t/p/original/hZ9536Zp07fO95fB6a166Pz8E.jpg`}  alt='s1' fill className='object-cover'/>
+        <Image src={`https://image.tmdb.org/t/p/original/xugEpZk9YQ0DIz1aFvH5HGkqpZK.jpg`}  alt='s1' fill className='object-cover'/>
         </MotionItem>
         <CardInfo
-        title={`Fight Club`}
-        desc={`A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground "fight clubs" forming in every town.`}
-        id={550}
+        title={`Lee Cronin's The Mummy`}
+        desc={`The young daughter of a journalist disappears into the desert without a trace—eight years later, the broken family is shocked when she is returned to them, as what should be a joyful reunion turns into a living nightmare.`}
+        id={1304313}
         />
         </>
   )},
@@ -55,12 +55,12 @@ export default function Hero({results}:{results:any[]}) {
     card:(
         <>
         <MotionItem initial={{opacity:0}} whileInView={{ opacity:1,transition:{duration:0.9} }}>
-        <Image src={`https://image.tmdb.org/t/p/original/9faGSFi5r4vtfj8g9S60id6m6XW.jpg`}  alt='s1' fill className='object-cover'/>
+        <Image src={'https://image.tmdb.org/t/p/original/4k99kV4R1bbbrsnjR205v91Xbin.jpg`}  alt='s1' fill className='object-cover'/>
         </MotionItem>
         <CardInfo
-        title={`Breaking Bad`}
-        desc={`Walter White, a New Mexico chemistry teacher, is diagnosed with stage III cancer and turns to manufacturing and selling methamphetamine with a former student, Jesse Pinkman, to secure his family's future.`}
-        id={1396}
+        title={`Obsession`}
+        desc={After breaking the mysterious \"One Wish Willow\" to win his crush's heart, a hopeless romantic finds himself getting exactly what he asked for but soon discovers that some desires come at a dark, sinister price.`}
+        id={1339713}
         />
         </>
   )},
@@ -68,12 +68,12 @@ export default function Hero({results}:{results:any[]}) {
     card:(
         <>
         <MotionItem initial={{opacity:0}} whileInView={{ opacity:1,transition:{duration:0.9} }}>
-        <Image src={`https://image.tmdb.org/t/p/original/mDfJG3LC3w668vG6mCcO1w0m8wF.jpg`} fill  alt='s1' className='object-cover'/>
+        <Image src={`https://image.tmdb.org/t/p/original/bq28ajZaoMyzEIm6REelqyqtEDZ.jpg`} fill  alt='s1' className='object-cover'/>
         </MotionItem>
         <CardInfo
-        title={`Avengers: Infinity War`}
-        desc={`As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos.`}
-        id={299536}
+        title={`The Boys`}
+        desc={`A group of vigilantes known informally as “The Boys” set out to take down corrupt superheroes with no more than blue-collar grit and a willingness to fight dirty.`}
+        id={76479}
         />
         </>
   )},
@@ -81,12 +81,12 @@ export default function Hero({results}:{results:any[]}) {
     card:(
         <>
         <MotionItem initial={{opacity:0}} whileInView={{ opacity:1,transition:{duration:0.9} }}>
-        <Image src={`https://image.tmdb.org/t/p/original/56v2aU606Y9hz9NuCYnZ6f6Sg0i.jpg`} fill  alt='s1' className='object-cover'/>
+        <Image src={`https://image.tmdb.org/t/p/original/oTE4lNs4PSG5iIWjqaTdCIFJ4Bs.jpg`} fill  alt='s1' className='object-cover'/>
         </MotionItem>
         <CardInfo
-        title={`Stranger Things`}
-        desc={`When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces and one strange little girl.`}
-        id={66732}
+        title={`Apex`}
+        desc={`A grieving woman pushing her limits on a solo adventure in the Australian wild is ensnared in a twisted game with a cunning killer who thinks she's prey.`}
+        id={1318447}
         />
         </>
   )},
@@ -94,12 +94,12 @@ export default function Hero({results}:{results:any[]}) {
     card:(
         <>
         <MotionItem initial={{opacity:0}} whileInView={{ opacity:1,transition:{duration:0.9} }}>
-        <Image src={`https://image.tmdb.org/t/p/original/6i7FFCH86wB08f969S6Iu979Y9q.jpg`}  alt='s1' fill className='object-cover'/>
+        <Image src={`https://image.tmdb.org/t/p/original/dd31qJxOarrHyGZwYsCzOjobQzP.jpg`}  alt='s1' fill className='object-cover'/>
         </MotionItem>
         <CardInfo
-        title={`The Hobbit: An Unexpected Journey`}
-        desc={`A reluctant Hobbit, Bilbo Baggins, sets out to the Lonely Mountain with a spirited group of dwarves to reclaim their mountain home, and the gold within it, from the dragon Smaug.`}
-        id={49051}
+        title={`Tom Clancy's Jack Ryan: Ghost War`}
+        desc={`Jack Ryan is reluctantly pulled back into espionage when an international covert mission unravels a deadly conspiracy. Racing against time, he joins CIA allies Mike November & James Greer and sharp MI6 officer Emma Marlowe to battle a rogue black-ops unit in a high-stakes, deeply personal fight.`}
+        id={1380291}
         />
         </>
   )},
