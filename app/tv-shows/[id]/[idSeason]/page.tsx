29855ff02a25873f2ed660f5adda327aa0505a42
Farse Fahmy <faresfahmysaeedahmed@gmail.com>
2026-06-07 08:35:47 +0300

    Remove console log from page component

--- app/tv-shows/[id]/[idSeason]/page.tsx
+++ app/tv-shows/[id]/[idSeason]/page.tsx
@@ -12,7 +12,6 @@ interface Props {
 export default async function page({ params }: Props) {
   const { id, idSeason} = await params;
   const detailsRes = await fetchCustom(`/tv/${id}/season/${idSeason}`)
-  console.log(detailsRes.data);
   return (
    <DetailsSeason results={detailsRes.data} id={Number(id)} />
   )
